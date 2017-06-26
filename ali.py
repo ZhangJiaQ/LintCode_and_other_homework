4267ef2aa4b2d47859796f8be0e5a6252906df87
Judge_Zhang <529326573@qq.com>
2017-06-26 22:29:25 +0800

    Update ali.py

--- ali.py
+++ ali.py
@@ -12,7 +12,7 @@ binary = FirefoxBinary("/usr/lib/firefox")
 driver = webdriver.Firefox(firefox_binary=binary)
 
 time.sleep(3)
-# 商户页面的url
+# 商户页面的URL
 # https://s.1688.com/company/company_search.htm?keywords=%BE%C6&n=y&spm=a260k.635.1998096057.d1
 url = 'https://s.1688.com/company/company_search.htm?keywords=%BE%C6&button_click=top&earseDirect=false&n=y'
 # 登录的url
@@ -21,7 +21,7 @@ login_url = 'https://login.taobao.com/member/login.jhtml'
 driver.get(login_url)
 # 睡眠5秒
 time.sleep(5)
-#
+#利用selenium的find_element方法选择帐号密码然后进行登录
 driver.find_element_by_css_selector("@TPL_username_1").send_keys("*******")
 driver.find_element_by_css_selector("@TPL_password_1").send_keys('*******码')
 driver.find_element_by_css_selector("@J_SubmitStatic").click()
@@ -29,20 +29,22 @@ driver.find_element_by_css_selector("@J_SubmitStatic").click()
 time.sleep(5)
 # 跳转到化工商户页面的url
 driver.get(url)
-
+# 防爬虫的UserAgent处理
 ua=UserAgent()
 
 for page in xrange(1, 100):
-    #
+    #寻找查找页面的所有商户URL
     title = driver.find_elements_by_css_selector(".company-list-item .list-item-title .a::attr(href)")
 
     for i in len(title):
         #https://shop1481648000066.1688.com
+        #进行URL合并，将匹配出来的与联系方式页面的URL进行合并
         contact_url = urljoin(title[i],"/page/contactinfo.htm?")
-
+        #对商户联系方式页面进行请求
         response = urllib.request.urlopen(contact_url,headers = {'User-Agent': ua.ramdom})
-
+        #
         html = response.text
+        #对返回页面数据进行分析，提取公司名称、联系方式、地址
 
 
 
