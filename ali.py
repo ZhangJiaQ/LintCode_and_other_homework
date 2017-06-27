d82b4d73738dc48363d4b2d73dec631a86826c1d
Judge_Zhang <529326573@qq.com>
2017-06-27 22:12:26 +0800

    Update ali.py

--- ali.py
+++ ali.py
@@ -9,51 +9,61 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Firefox()
 
 time.sleep(3)
+
 # 商户页面的URL
 # https://s.1688.com/company/company_search.htm?keywords=%BE%C6&n=y&spm=a260k.635.1998096057.d1
 url = 'https://s.1688.com/company/company_search.htm?keywords=%BE%C6'
+
 # 登录的url
 login_url = 'https://login.taobao.com/member/login.jhtml'
+
 # 跳转到登录页面
 driver.get(login_url)
 driver.maximize_window()
+
 # 睡眠5秒
 time.sleep(5)
+
 #利用selenium的find_element方法选择帐号密码然后进行登录
 element=WebDriverWait(driver,60).until(lambda driver :driver.find_element_by_xpath("//*[@id='J_Quick2Static']"))
 element.click()
-driver.find_element_by_name("TPL_username").send_keys('3382315515@qq.com')#3382315515@qq.com
+driver.find_element_by_name("TPL_username").send_keys('***')#***
 driver.find_element_by_name("TPL_password").clear()
-driver.find_element_by_name("TPL_password").send_keys('zmq2006zz')#zmq2006zz
+driver.find_element_by_name("TPL_password").send_keys('***')#***
 driver.find_element_by_xpath("//*[@id='J_SubmitStatic']").click()
+
+#异地登录，需要输入手机验证码
 time.sleep(30)
 
 # 跳转到化工商户页面的url
 driver.get(url)
-# 防爬虫的UserAgent处理
 
-cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
+# 防爬虫的UserAgent处理
 ua=UserAgent()
+
+#利用Selunium将Cookie保存下来，便于Requests使用
+cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
 cookiestr = ';'.join(item for item in cookie)
-cookies={}#初始化cookies字典变量
+cookies={}
 for line in cookiestr.split(';'):   #按照字符：进行划分读取
     #其设置为1就会把字符串拆分成2份
     name,value=line.strip().split('=',1)
     cookies[name]=value
 
+#进行数据爬取
 for page in range(1, 100):
-    #
+    #选择爬取每个商户主页的URL
     title = driver.find_elements_by_css_selector("a[class=list-item-title-text]")
-
+    #爬取每个商户联系方式页面，并解析提取
     for i in range(len(title)):
         contact_url = title[i].get_attribute("href") + 'page/contactinfo.htm'
         #print(contact_url)
         headers = {'User-Agent': ua.random, 'Accept': '*/*', 'Referer': 'http://www.google.com'}
         r = requests.get(contact_url, headers=headers, cookies=cookies)
         contact_content = r.text
+        pass
 
-
-
+    #进行下一页的爬取
     next_url = "https://s.1688.com/company/company_search.htm?sortType=pop&pageSize=30&keywords=%BE%C6&offset=3&beginPage={0}".format(i)
     driver.get(next_url)
 
